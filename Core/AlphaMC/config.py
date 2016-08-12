de20d58439f742a1ecbd645f9f15e90b17bd0eff
AlphaMCPE <dtbgaming101@gmail.com>
2016-08-12 17:41:24 +0000

    Add files via upload

---
+++ Core/AlphaMC/config.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#AlphaMc Config File
+#Version Of Config: 1
+#All Options Are <TRUE OR FALSE>
+
+#                   SERVER
+#Redstone
+redstone = 'true'
+#Weather
+weather = 'true'
+#Firespread
+firespread = 'false'
+#login message
+loginmessage = '@player joined the game'
+#logout message
+logoutmessage = '@player joined the game'
+#server file location
+root = '/home/pi/Desktop/AlphMc/'
+#                    NETHER
+nether = 'false'
+
+#                     PLAYER
+#Hunger
+hunger = 'true'
+#experience
+experience = 'true'
+
+#                   ENCHANMENT
+enchanment = 'true'
+
+#                       AI
+#AI switch
+ai = 'false'
+#Zombie'AI
+zombieai = 'false'
+#PigZombie AI
+pigzombieai = 'false'
+#cow and mushroom cow AI
+cowsai = 'false'
+#Chicken AI
+chickenai = 'false'
+#Sheep AI
+sheepai = 'false'
+#Pig AI
+pigai = 'false'
+#Skeleton AI
+skeletonai = 'false'
+#Creeper AI
+creeperai = 'false'
+#Iron golem AI
+irongolemai = 'false'
+#Snow golem AI
+snowgolemai = 'false'
+#Creeper block explode
+creeperexplodeai = 'false'
+#Mob auto generate
+mobgenerateai = 'false'
