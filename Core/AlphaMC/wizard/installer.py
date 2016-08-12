8e9293481e79b2eb4a24fd5f3ea603f90f1b4f4d
AlphaMCPE <dtbgaming101@gmail.com>
2016-08-12 03:33:32 +0000

    Update installer.py

--- Core/AlphaMC/wizard/installer.py
+++ Core/AlphaMC/wizard/installer.py
@@ -1,28 +1,45 @@
-# AlphaMC Installer
-# runs on first boot
+#AlphaMc Installer
+#runs on first boot
+import os
+from shutil import copyfile
 
-print(" ")
-print("Welcome to AlphaMC Server Software! You are going to setup the server now!")
-print(" ")
-print("Please pick a language")
-lang = raw_input("language eng-> English: ")
+print " "
+print "Welcome to AlphaMc Server Software! You are going to setup the server now!"
+print " "
+install = True
+while install:
+    choice = raw_input("Please accept the license. You are able to download and share this codes but cannot use in your builds without giving credit to owner. Y/N: ")
+    if choice == "Y":
+        print "License agreed!"
+        install = False
+        print " "
+    else:
+        print 'Please accept licence'
 
-if lang == 'eng':
-    print(lang, 'is picked')
-else:
-    print('invalid language! try setup again!')
-    exit()
+print "	"
+mcpename = raw_input("Please put in your mcpe server name: ")
+print'You picked', mcpename
 print "	"
 
-mcpename = raw_input("Please put in your mcpe server name: ")
-print('You picked', mcpename)
-print("")
-
-print('Do you accept the license? These codes are open for others to distribiute but to give full credit to original owners?')
-license = raw_input('Do you accept this? ')
-
-if license == 'yes':
-	print('license is accepted!')
-else:
-	print 'Please acept the license! Try setup again!'
-	exit()
+install = True
+while install:
+    license = raw_input('Do you want to skip the installer? Y/N ')
+    if license == 'Y':
+        install = False
+    else:
+        print 'You will setup the server now!'
+        print ' '
+        
+	
+os.chdir('/home/pi/Desktop/AlphaMc')
+if not os.path.exists('worlds'):
+    os.makedirs('worlds')
+if not os.path.exists('plugins'):
+    os.makedirs('plugins')
+if not os.path.exists('crashes'):
+    os.makedirs('crashes')
+if not os.path.exists('players'):
+    os.makedirs('players')
+if not os.path.isfile('config.py'):
+    copyfile('/home/pi/Desktop/AlphaMc/Core/AlphaMc/res/config.py', '/home/pi/Desktop/AlphaMc/config.py')
+print 'Setup complete!'
